ac2bd754e1f3069f70e36ae5fffffdd4ddf12aa0
navaneeth-algorithm <navaneethb807@gmail.com>
2019-08-17 16:19:26 +0530

    Index

--- index.php
+++ index.php
@@ -12,98 +12,34 @@
 </head>
 <body class="w3-display-container w3-light-gray">
 
-        <div id="WebContent"  style="width:70%;margin-left:170px;" class="w3-white">
+        <div id="WebContent"  style="width:70%;margin-left:210px;" class="w3-white">
 
-                <header>    
-                    <div class="w3-container w3-white w3-row w3-margin">
-                        <!-- Navigation Bar Container -->
 
-                        <div class="w3-row w3-margin">
-                            <!-- Row containing Logo  -->
-                            <div class="w3-col s6"><img src="images/mlogo.jpg" heigth="100px" width="100px"></div>
-                        </div>
+            <?php include('include/header.php'); ?>
 
-                        <div class="w3-row w3-margin">
-                            <!-- Row containing contact Details  -->
-                            <div class="w3-col s6" >
-                                <!-- Name of company -->
-                                <span>  Sadhguru  Souharda Sahakari Limited</span>
-                            </div>
-
-                            <div class="w3-col s6" >
-                                <!-- Contact Details -->
-                                <span>Contact +91  9448327884 </span>
-                            </div>
-
-                        </div>
-
-                        <div class="w3-bar w3-row w3-margin">
-                            <!-- Row containing Navigation Details  -->
-                            <a href="#" class="w3-bar-item w3-button">Home</a>
-                            <a href="#" class="w3-bar-item w3-button">Link 1</a>
-                            <a href="#" class="w3-bar-item w3-button">Link 2</a>
-                            <a href="#" class="w3-bar-item w3-button">Link 3</a>
-                        </div>
-
-                    </div>
-                </header>
-
-                <div class="w3-row w3-padding w3-light-gray" style="width:100%">
-                    <!-- Here Goes Line Breaking between Navigation and WebContent  -->
-                </div>
 
                 <div class="w3-row w3-padding w3-white" style="width:97%;margin-left:15px;">
-                    <!-- Here Goes Content of web   -->
-                
-                </div>
+                    <!-- Here Goes Middle Content of web   -->
 
-                <footer style="text-align:center">
-                    <!-- FooterPage -->
-                    <div class="w3-row w3-margin">
-                        <div class="w3-col s6">
-                            <!--Here Goes Map Information -->
-                            <h4>
-                                Google Map 
-                            </h4>
-                            <div>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3882.3715592215117!2d74.7399860147288!3d13.327147990623374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbcbb0cfec504f5%3A0xd853c0e4e70612d5!2sSadhguru!5e0!3m2!1sen!2sin!4v1566026704204!5m2!1sen!2sin" 
-                                width="300" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
-                            </div>
-                        </div>
-                        <div class="w3-col s6 w3-padding" >
-                                    <h4>
-                                        Contacts
-                                    </h4>
-                                    <p>
-                                        Sadhguru Skill Development Centre
-                                    </p>
-                                    <p>
-                                        Kinnimulki,
-                                    </p>
-                                    <p>
-                                        Udupi – 576101 
-                                    </p>
-                                    <p>
-                                        Email: sadhguruudupi@gmail.com 
-                                    </p>
-                                    <p>
-                                        Phone: +91 9448327884
-                                    </p>
-                                    <h4>
-                                        Follow Us On
-                                    </h4>
-                                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                                    <i class="fa fa-twitter"  aria-hidden="true"></i>
-                                    <i class="fa fa-linkedin" aria-hidden="true"></i>
-                                    <i class="fa fa-dribbble" aria-hidden="true"></i>
-                        </div>
-
-                    </div>
+                    <!-- Banner Include -->
+                    <?php  include('include/banner.php'); ?>
 
                     <div class="w3-row">
-                        © 2019 All Rights Reserved. Developed by Sadhguru Souharda Sahakari Limited.                
+                        <div class="w3-col s9">
+                            <!-- Here Goes Content Excludeing banner & right bar Con-->
+                            HelloWorld
+                        </div>
+                        <div class="w3-col s3">
+                            <!-- Here Goes Right bar -->
+                            <?php include('include/rightbar.php');  ?>
+                        </div>
                     </div>
-                </footer>
+
+                </div>
+
+            
+            <?php include('include/footer.php');  ?>
+
 
         </div>
 

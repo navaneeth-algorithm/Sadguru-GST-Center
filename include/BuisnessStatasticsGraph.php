ab649b99e4a9591f81eba36a4d7ebed2def6a9e2
Algorithm <navaneethb807@gmail.com>
2019-09-04 18:16:22 +0530

    LastYEar

--- include/BuisnessStatasticsGraph.php
+++ include/BuisnessStatasticsGraph.php
@@ -1,8 +1,58 @@
 
 <div class="w3-card w3-padding">
-		<?php
-		
-		$dataPoints1 = array(
+	<?php
+		include("dbConnect.php");
+		$buisnessStatasticQuery="SELECT * FROM `BuisnessStatastics`";
+		$buisnessStatasticQueryResult= mysqli_query($conn, $buisnessStatasticQuery) or die(mysqli_error($conn));  
+		if(mysqli_num_rows($buisnessStatasticQueryResult))
+		{
+		// $row=mysqli_fetch_assoc($suc);
+		$dataPoints = array();
+		$noData = 0;
+			while($buisnessStatasticRow = mysqli_fetch_assoc($buisnessStatasticQueryResult))
+			{
+				$buisnessStatasticName = $buisnessStatasticRow['Name'];
+				$buisnessStatasticId= $buisnessStatasticRow['Id'];
+				$temp = array();
+				$buisnessStatasticYearQuery="SELECT * FROM `BuisnessStatasticsYear`";
+				$buisnessStatasticYearQueryResult= mysqli_query($conn, $buisnessStatasticYearQuery) or die(mysqli_error($conn));
+				if(mysqli_num_rows($buisnessStatasticYearQueryResult))
+				{
+					while($buisnessStatasticYearRow = mysqli_fetch_assoc($buisnessStatasticYearQueryResult))
+					{
+						$buisnessStataticYearId = $buisnessStatasticYearRow['Id'];
+						$buisnessStataticYearName = $buisnessStatasticYearRow['Year'];
+						$buisnessStatasticDataQuery="SELECT * FROM `BuisnessStatasticsData` WHERE NameId=$buisnessStatasticId AND YearId=$buisnessStataticYearId";
+						$buisnessStatasticDataQueryResult= mysqli_query($conn, $buisnessStatasticDataQuery) or die(mysqli_error($conn));
+						if(mysqli_num_rows($buisnessStatasticDataQueryResult))
+						{
+							while($buisnessStatasticDataRow = mysqli_fetch_assoc($buisnessStatasticDataQueryResult))
+							{
+								$buisnessStatasticData = $buisnessStatasticDataRow['Data'];
+								$tempArray["label"] = " ".$buisnessStataticYearId;
+								$tempArray["y"] = $buisnessStatasticData;
+							 
+							}
+						}
+						array_push($temp,$tempArray);
+						
+					}
+					
+					
+				}
+				$dataPoints[$noData++] = $temp;
+
+			}
+
+		}
+			?>
+	  <?php
+		$r = 0;
+		//print_r($dataPoints);
+		/*
+		$dataPoints[] = array();
+
+		$dataPoints[0] = array(
 			array("label"=> "2014", "y"=> 2016456 ),
 			array("label"=> "2015", "y"=> 1985801 ),
 			array("label"=> "2016", "y"=> 1755904 ),
@@ -10,7 +60,7 @@
 			array("label"=> "2018", "y"=> 1733430 ),
 		);
 		
-		$dataPoints2 = array(
+		$dataPoints[1] = array(
 			array("label"=> "2014", "y"=> 49505 ),
 			array("label"=> "2015", "y"=> 31917 ),
 			array("label"=> "2016", "y"=> 25972 ),
@@ -18,7 +68,7 @@
 			array("label"=> "2018", "y"=> 16086 ),
 		);
 		
-		$dataPoints3 = array(
+		$dataPoints[2] = array(
 			array("label"=> "2014", "y"=> 896590 ),
 			array("label"=> "2015", "y"=> 882981 ),
 			array("label"=> "2016", "y"=> 920979 ),
@@ -26,7 +76,7 @@
 			array("label"=> "2018", "y"=> 1013689 ),
 		);
 		
-		$dataPoints4 = array(
+		$dataPoints[3] = array(
 			array("label"=> "2014", "y"=> 806425 ),
 			array("label"=> "2015", "y"=> 806208 ),
 			array("label"=> "2016", "y"=> 798855 ),
@@ -34,7 +84,7 @@
 			array("label"=> "2018", "y"=> 790204 ),
 		);
 		
-		$dataPoints5 = array(
+		$dataPoints[4] = array(
 			array("label"=> "2014", "y"=> 247510 ),
 			array("label"=> "2015", "y"=> 254831 ),
 			array("label"=> "2016", "y"=> 273445 ),
@@ -42,21 +92,21 @@
 			array("label"=> "2018", "y"=> 319355 ),
 		);
 		
-		$dataPoints6 = array(
+		$dataPoints[5] = array(
 			array("label"=> "2014", "y"=> 612 ),
 			array("label"=> "2015", "y"=> 864 ),
 			array("label"=> "2016", "y"=> 891 ),
 			array("label"=> "2017", "y"=> 1212 ),
 			array("label"=> "2018", "y"=> 1818 ),
 		);
-		$dataPoints7 = array(
+		$dataPoints[6] = array(
 			array("label"=> "2014", "y"=> 312 ),
 			array("label"=> "2015", "y"=> 321 ),
 			array("label"=> "2016", "y"=> 321 ),
 			array("label"=> "2017", "y"=> 122 ),
 			array("label"=> "2018", "y"=> 645 ),
 		);
-		
+		*/
 		?>
 		<script>
 		window.onload = function () {
@@ -86,14 +136,14 @@
 				showInLegend: true,
 				visible: false,
 				yValueFormatString: "#,##0 no's",
-				dataPoints: <?php echo json_encode($dataPoints1, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[0], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
 				name: "Share Capital",
 				showInLegend: true,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[1], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
@@ -101,7 +151,7 @@
 				showInLegend: true,
 				visible: false,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[2], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
@@ -109,7 +159,7 @@
 				showInLegend: true,
 				visible: false,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints4, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[3], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
@@ -117,21 +167,21 @@
 				showInLegend: true,
 				visible: false,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints5, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[4], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
 				name: "Profit",
 				showInLegend: true,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints6, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[5], JSON_NUMERIC_CHECK); ?>
 			},
 			{
 				type: "stackedArea",
 				name: "Dividend",
 				showInLegend: true,
 				yValueFormatString: "#,##0 lakhs",
-				dataPoints: <?php echo json_encode($dataPoints7, JSON_NUMERIC_CHECK); ?>
+				dataPoints: <?php echo json_encode($dataPoints[6], JSON_NUMERIC_CHECK); ?>
 			}
 			
 			]
